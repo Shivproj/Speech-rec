60ae07df55a7c872e65c8a5716208373f83a409f
Shivproj <shivindrakanti@gmail.com>
2022-12-02 14:46:36 +0530

    two fields done

--- index.js
+++ index.js
@@ -4,36 +4,38 @@ const label1 = document.getElementById("Name").innerHTML;
 const label2 = document.getElementById("Age").innerHTML;
 const btn = document.getElementById("submit");
 btn.addEventListener("click", () =>{
-    console.log(array);
+    
 })
-let array = [];
+let details = {Name:"",Age:""};
 
 window.SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new window.SpeechRecognition();
 recognition.interimResult = true;
-recognition.addEventListener("result", (e) => {
-  inp1.value = e.results[0][0].transcript;
-  console.log(inp1.value);
-});
 
-function textToSpeech(label1) {
-  let utterance = new SpeechSynthesisUtterance("Please" + label1);
+function textToSpeech(label) {
+  let utterance = new SpeechSynthesisUtterance("Please" + label);
   speechSynthesis.speak(utterance);
 }
 inp1.addEventListener("click", () => {
   recognition.addEventListener("result", (e) => {
-    array.push(e.results[0][0].transcript);
-    inp1.value = array[0];
+    if(details.Name) {e.results[0].pop()}    
+    details.Name = e.results[0][0].transcript; 
+    inp1.value=details.Name;
+ 
   });
-  if (!inp1.value) {textToSpeech(label1);
+  if (!inp1.value) {
+    textToSpeech(label1);
   recognition.start();}
   else recognition.stop()
 });
 inp2.addEventListener("click", () => {
   recognition.addEventListener("result", (e) => {
-    array.push(e.results[0][0].transcript);
-    inp2.value = array[1];
+    if(details.Age) {e.results[0].pop()}
+  details.Age=e.results[0][0].transcript;
+  inp2.value=details.Age;
+
+   
   });
   if (!inp2.value) 
   {textToSpeech(label2);
